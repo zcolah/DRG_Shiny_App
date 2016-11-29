baa6d952389a7909eb51e52cd34ae6f4feeb4038
Steven Bishop <scbish75@msn.com>
2016-11-28 21:55:47 -0800

    can't figure out what's wrong with the map. can someone help?

---
+++ scripts/choropleth.r
@@ -0,0 +1,25 @@
+library(plotly)
+library(dplyr)
+hospital.data <- read.csv("data/hospital_data.csv", stringsAsFactors = F)
+hospital.data.shortened <- hospital.data %>%
+  select(Provider.City, Provider.State, Average.Covered.Charges) %>% 
+  group_by(Provider.State) %>% 
+  summarize(State.Covered.Charges = mean(as.numeric(gsub("\\$", "", Average.Covered.Charges))))
+makeChoropleth <- function(data){
+  g <- list(
+    scope = 'usa',
+    projection = list(type = 'albers usa'),
+    showlakes = TRUE,
+    lakecolor = toRGB('white')
+  )
+  plot_geo(data, locationmode = 'USA-states') %>%
+    add_trace(
+      z = ~State.Covered.Charges, text = ~State.Covered.Charges, locations = ~Provider.State,
+      color = ~State.Covered.Charges, colors = 'Purples'
+    ) %>%
+    colorbar(title = "Covered Charges") %>%
+    layout(
+      title = 'Average Covered Medical Charges by State<br>(Hover for breakdown)',
+      geo = g
+    )
+}
