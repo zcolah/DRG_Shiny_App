35eadc52d44f7d9ce1d00ef8cffe86586ba07722
Steven Bishop <scbish75@msn.com>
2016-11-28 23:55:14 -0800

    removed washington DC, still doesnt work

--- scripts/choropleth.r
+++ scripts/choropleth.r
@@ -4,7 +4,8 @@ hospital.data <- read.csv("data/hospital_data.csv", stringsAsFactors = F)
 hospital.data.shortened <- hospital.data %>%
   select(Provider.City, Provider.State, Average.Covered.Charges) %>% 
   group_by(Provider.State) %>% 
-  summarize(State.Covered.Charges = mean(as.numeric(gsub("\\$", "", Average.Covered.Charges))))
+  summarize(State.Covered.Charges = mean(as.numeric(gsub("\\$", "", Average.Covered.Charges)))) %>% 
+  filter(Provider.State != "DC")
 makeChoropleth <- function(data){
   g <- list(
     scope = 'usa',
