8a9f904023cd43e0afb4e1737bc1d36698eaeb2b
scbish <scbish75@msn.com>
2016-11-30 16:11:40 -0800

    Updated Stuff

--- scripts/choropleth.r
+++ scripts/choropleth.r
@@ -1,6 +1,6 @@
 library(plotly)
 library(dplyr)
-makeCountryChoropleth <- function(data){
+makeStateChoropleth <- function(data){
   g <- list(
     scope = 'usa',
     projection = list(type = 'albers usa'),
@@ -15,6 +15,25 @@ makeCountryChoropleth <- function(data){
     colorbar(title = "Average Covered Charges / Average Total Payments") %>%
     layout(
       title = 'Ratio of Average Covered Medical Charges to Average Total Payments by State<br>(Hover for breakdown)',
+      geo = g
+    )
+}
+
+makeCountyChoropleth <- function(data){
+  g <- list(
+    scope = 'usa',
+    projection = list(type = 'albers usa'),
+    showlakes = TRUE,
+    lakecolor = toRGB('white')
+  )
+  plot_geo(data, locationmode = 'USA-counties') %>%
+    add_trace(
+      z = ~State.Covered.Charges, text = ~State.Covered.Charges, locations = ~county,
+      color = ~State.Covered.Charges, colors = 'Purples'
+    ) %>%
+    colorbar(title = "Average Covered Charges / Average Total Payments") %>%
+    layout(
+      title = 'Ratio of Average Covered Medical Charges to Average Total Payments by County<br>(Hover for breakdown)',
       geo = g
     )
 }
