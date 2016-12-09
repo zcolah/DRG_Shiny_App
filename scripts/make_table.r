218b85c700d6b3f4f1372f27603857035994d58b
scbish <scbish75@msn.com>
2016-12-08 20:03:46 -0800

    added comments

--- scripts/make_table.r
+++ scripts/make_table.r
@@ -7,8 +7,8 @@ makeTable <- function(data, drg, payment){
     filter(DRG.Definition == drg, Average.Total.Payments <= payment[[2]] &
              DRG.Definition == drg, Average.Total.Payments >= payment[[1]]) %>% 
     select(Provider.City, Provider.Name, Average.Covered.Charges, Average.Total.Payments, Average.Medicare.Payments) %>% 
-    mutate(City = Provider.City, Hospital = Provider.Name, "Average Covered Charges" = Average.Covered.Charges, "Average Total Payments" = paste0("$", Average.Total.Payments), "Average Medicare Payments" = Average.Medicare.Payments) %>% 
-    select(-Provider.City, -Provider.Name, -Average.Covered.Charges, -Average.Total.Payments, -Average.Medicare.Payments)
+    mutate(City = Provider.City, Hospital = Provider.Name, "Average Covered Charges" = Average.Covered.Charges, "Average Total Payments" = paste0("$", Average.Total.Payments), "Average Medicare Payments" = Average.Medicare.Payments) %>%  # This will name the columns how I want it to
+    select(-Provider.City, -Provider.Name, -Average.Covered.Charges, -Average.Total.Payments, -Average.Medicare.Payments)  # This will get rid of all the columns I don't want
   
   return(data)
 }
